3e5ea6c2240e948c8551b4477baacc34cd4b5872
AnisLcool <42942454+AnisLcool@users.noreply.github.com>
2020-04-17 19:41:25 +0100

    style , script, html , added the code for roll button

--- app.js
+++ app.js
@@ -8,3 +8,49 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+// setting the variables 
+let scores, round_score, active_player,img_container,  tagScore, roll_btn, dice_image;
+
+score = [0,0];
+round_score = 0;
+active_player = 0 ; // player 0 will have the first turn
+
+img_container = document.querySelector(".dice-img");
+tagScore = document.querySelector(`#current${active_player + 1}`)
+
+
+img_container.style.display = "none"; // hiding the dice
+
+dice_image = img_container.children[0]; // retrieving the img
+
+  
+
+let roll_dice = (event) => {
+    
+    let dice_number = Math.floor(Math.random() * 6) + 1; // random number between 1 and 6 inclusively
+    
+    // setting the src attribute to retrieve the dice img that matches the dice number
+    dice_image.src = `dice-${dice_number}.png`; 
+    img_container.style.display = "block"; // displaying the img
+
+    round_score += dice_number; // adding the dice number to round score
+
+    // setting the round score, if the dice number is 1 , reset the round score to 0
+    if(dice_number !== 1){
+        tagScore.textContent = round_score;
+    }
+    else{
+        round_score = 0;
+        tagScore.textContent = 0;
+    }
+    
+}
+
+roll_btn = document.querySelector(".btn-roll");
+roll_btn.addEventListener("click", roll_dice);
+
+
+
+
+
